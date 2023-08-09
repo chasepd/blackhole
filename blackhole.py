f038475d2f30fd3f4d0395752c70dd961b7b1b3a
Chase Davies <chase.davies@sproutsocial.com>
2023-08-09 17:16:27 -0600

    Slightly modified comments for consistency

--- blackhole.py
+++ blackhole.py
@@ -20,11 +20,11 @@ def handle_client(client_socket, client_address):
     print(f"Connection received from {client_address}")
     handle_delay(client_address[0])
     try:
-        # You can now interact with the client using client_socket, e.g.:
+        ### You can now interact with the client using client_socket, e.g.:
         message = fake_vulnerability_response()
         client_socket.sendall(message.encode('utf-8'))
     finally:
-        # Close the client socket when done
+        ### Close the client socket when done
         client_socket.close()
 
 
@@ -32,9 +32,9 @@ def handle_client(client_socket, client_address):
 def dynamic_port_server(min_port=10000, max_port=65535, port_list=[]):
     running = True    
     while running:
-        server_sockets = [] # List to store the 10 server sockets
+        server_sockets = [] ### List to store the 10 server sockets
         try:
-            # Create and run 10 server sockets
+            ### Create and run 10 server sockets
             for _ in range(10):
                 port = -1
                 if len(port_list) == 0:
@@ -46,28 +46,27 @@ def dynamic_port_server(min_port=10000, max_port=65535, port_list=[]):
                 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 server_socket.bind(('0.0.0.0', port))
                 server_socket.listen(1)
-                server_socket.settimeout(1) # Sets a timeout of 1 second
+                server_socket.settimeout(1)
                 print(f"Listening on port {port}...")
                 server_sockets.append(server_socket)
             
-            # Keep track of the time when the server started
+            ### Keep track of the time when the server started
             curr_time = time.time()
 
-            # Run on current ports for 5 seconds
+            ### Run on current ports for 5 seconds
             while time.time() - curr_time < 5:
                 for server_socket in server_sockets:
                     try:
-                        # Accept a connection
                         client_socket, client_address = server_socket.accept()
                     except socket.timeout:
                         continue
 
-                    # Start a new thread to handle the connection
+                    ### Start a new thread to handle the connection
                     client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address), daemon=True)
                     client_thread.start()
         except KeyboardInterrupt:
             print("Closing server sockets...")
-            # Close all server sockets
+            ### Close all server sockets
             for server_socket in server_sockets:
                 server_socket.close()
             running = False
@@ -113,14 +112,14 @@ def main():
 
     args = argparser.parse_args()
 
-    # Count how many port arguments were specified
+    ### Count how many port arguments were specified
     port_conditions = sum([
-        bool(args.port_list), # Check if the list is not empty
+        bool(args.port_list), ### Check if the list is not empty
         (args.min_port is not None and args.max_port is not None),
         (args.min_port is None and args.max_port is None and not args.port_list)
     ])
 
-    # Check that exactly one port argument was specified
+    ### Check that exactly one port argument was specified
     if port_conditions != 1:
         argparser.error("Invalid port specification; only one of -p, -m/-M, or -l can be specified")
 
